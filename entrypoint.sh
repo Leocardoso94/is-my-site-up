754c9879a4b6574ffbb86139057d8234b005ee76
Leonardo Cardoso <leocardosoti@gmail.com>
2020-03-22 14:33:45 -0300

    create actions file

---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+FAIL_CODE=6
+
+check_status() {
+    LRED="\033[1;31m"   # Light Red
+    LGREEN="\033[1;32m" # Light Green
+    NC='\033[0m'        # No Color
+
+    curl -sf "${1}" >/dev/null
+
+    if [ ! $? = ${FAIL_CODE} ]; then
+        echo -e "${LGREEN}${1} is online${NC}"
+    else
+        echo -e "${LRED}${1} is down${NC}"
+        exit 1
+    fi
+}
+
+check_status "${1}"
